00cefc693705ee769ce9dc82215d8c7019be76f2
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-20 14:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+const driver = [];
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  //const driver = "Sam";
+  return Object.assign(driver, "Sam", "11 Broadway")
+
+
+};
+
+
+//driver.address;

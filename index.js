f9c1cda216207e920fcea766df13ea396b389eda
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-20 14:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const driver = {driver:"driver"}
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newPair = {}; //brackets means you're creating an object
   newPair[key] = value; //how you assign your key and value to your object
-  //ex of key => name: "Caroline"
+  //ex of key and value => name: "Caroline"
   hateObjects = Object.assign({}, driver, newPair);
   return hateObjects
 };
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-
+  driver[key] = value;
+  return driver;
+  
 }
